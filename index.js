62970bee397d3efb17c83643d000a51e12522b4c
Ixchell Tingling <ixchell.tingling@gmail.com>
2018-03-18 14:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + "Neil deGrasse Tyson" + "!";
 
 var topic = "space";
 
-var conversation = "Let's talk about" + ${topic};
+var conversation = "Let's talk about ${topic}"
